e50aa985812ee541973940c5e21d67e7437ed1c4
BastienSch <43954016+BastienSch@users.noreply.github.com>
2019-03-19 09:08:02 +0100

    Add files via upload

---
+++ require/head.php
@@ -0,0 +1,11 @@
+<head>
+    <title>TorrentSave</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/css/base.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css.map">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css.map">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
---
+++ require/navbar.php
@@ -0,0 +1,10 @@
+<aside class="column column-20">
+  	<h2>Torrent Save</h2>
+  	<br/>
+ 	<a class="button" href="index.php"><i class="fa fa-list-ul fa-fw margin-right"></i>Liste des Torrents</a>
+ 	<a class="button" href="share.php"><i class="fa fa-list-ul fa-fw margin-right"></i>Liste des Sauvegardes</a>
+ 	<a class="button" href="pair.php"><i class="fa fa-list-ul fa-fw margin-right"></i>Liste des Pairs</a>
+ 	<a class="button" href="newPairs.php"><i class="fa fa-check fa-fw margin-right"></i>Validation des Pairs</a>
+ 	<a class="button" href="login.php?logout"><i class="fas fa-sign-out-alt fa-fw margin-right"></i>Déconnexion</a>
+
+</aside>
